7b348005c9aecfcee273f7c5c0428efa36df6622
Mihaiela Nemes <mihaiela.nemes@stellenanzeigen.de>
2020-10-17 15:58:34 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -4,23 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit the code <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, Mia
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
+      </div>
   );
 }
+
+export default App;
 
 export default App;
